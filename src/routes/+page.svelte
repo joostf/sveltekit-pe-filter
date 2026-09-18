5728940454170d5f71c74dc7aff65b972f5f514d
joostf <j.faber@hva.nl>
2026-09-18 15:29:38 +0200

    added view transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,26 @@
 <script>
-    import { goto } from '$app/navigation'
+    import { goto, onNavigate } from '$app/navigation'
+    import { browser } from '$app/environment'
+
     let { data } = $props()
-    let type = $state(data.type ?? "")
+    let type = $derived(data.type ?? "")
+
+    onNavigate((navigation) => {
+        {
+            const reduceMotion = window.matchMedia(
+                '(prefers-reduced-motion: reduce)'
+            ).matches
+
+            if (reduceMotion || !document.startViewTransition) return
+        }
+        
+        return new Promise((resolve) => {
+            document.startViewTransition(async () => {
+                resolve()
+                await navigation.complete
+            })
+        })
+    })
 
     function onChange() {
         const params = new URLSearchParams()
@@ -32,12 +51,16 @@
                     <option value="vis">met vis 🐟</option>
                 </select>
             </label>
-
-            <button type="submit">Filter pizza's</button>  
+            {#if !browser}
+                <button type="submit">Filter pizza's</button>  
+            {/if}
         </form>
 
-        {#each data.pizzas as pizza}
-            <article class="pizza-card">
+        {#each data.pizzas as pizza (pizza.id)}
+            <article
+                class="pizza-card"
+                style={`view-transition-name: pizza-card-${pizza.id}`}
+            >
                 <h2>{pizza.name}</h2>
                 <p>Prijs: €{pizza.price}</p>
                 <p>Besteld: {pizza.ordered}</p>
@@ -128,6 +151,7 @@
     }
 
     .pizza-card {
+        view-transition-class: pizza-card;
         background-color: var(--pizza-card-bg);
         border: 2px solid var(--pizza-card-border);
         border-radius: var(--pizza-card-radius);
@@ -149,12 +173,29 @@
         color: var(--pizza-sauce);
     }
 
-    .veggie {
-        font-weight: bold;
-        color: var(--pizza-veggie);
-    }
-
     .pizza-card p {
         margin: 0.3rem 0;
+    }
+
+    @keyframes pizza-in {
+        from {
+            opacity: 0;
+            transform: translateY(3rem) scale(0.25);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0)  scale(1);
+        }
+    }
+
+    ::view-transition-new(.pizza-card) {
+        animation: pizza-in 250ms ease-out both;
+        transform-origin: left bottom;
+    }
+
+    @media (prefers-reduced-motion: reduce) {
+        ::view-transition-new(.pizza-card) {
+            animation: none;
+        }
     }
 </style>
