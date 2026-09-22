cb219d25ac3b9aa50262dde5bff8f76798908dc4
joostf <j.faber@hva.nl>
2026-09-22 10:07:02 +0200

    Remove redundant comment

--- src/lib/asset.js
+++ src/lib/asset.js
@@ -1,6 +1,5 @@
 const ASSET_URL = 'https://fdnd-agency.directus.app/assets'
 
-/** @param {string | undefined} image */
 export function assetUrl(image) {
   return image ? `${ASSET_URL}/${image}` : ''
 }
