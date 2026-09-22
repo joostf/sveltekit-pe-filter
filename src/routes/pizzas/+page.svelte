2e45b514cc826b1e2ca258aef4ef43e8406cb690
joostf <j.faber@hva.nl>
2026-09-22 16:07:32 +0200

    More concise css nesting

--- src/routes/pizzas/+page.svelte
+++ src/routes/pizzas/+page.svelte
@@ -8,15 +8,5 @@
 <svelte:head><title>Alle nerdy pizza's | Nerdy Pizzas</title></svelte:head>
 
 <SiteHeader selectedType={data.selectedType} selectedSort={data.selectedSort} />
-<main><PizzaList {...data} /></main>
+<PizzaList {...data} />
 
-<style>
-  main {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-    width: min(100%, 96rem);
-    margin-inline: auto;
-    padding-bottom: 1rem;
-  }
-</style>
